73aa269c4d2b58ea03c23b493bfe4b5bc585f1f8
mklaric91 <magdalena_klaric@hotmail.com>
2024-04-08 11:38:01 +0200

    submission

--- main.py
+++ main.py
@@ -2,9 +2,40 @@
 PowerOfTen
 """
 # Provide your solution here
+input_str = input("Enter a max 3 digit number ")
+is_less_than_0 = number > 0
+
+if number > 0:
+    print(f"number cannot be negative {is_less_than_0}")
+
+elif number < 999:
+    print (f"number has more than 3 digits")
+
+else
+
+
+
 
 
 """
 Cash Box
 """
 # Provide your solution here
+amount = 50
+
+payment = input("To pay: 50")
+received = input ()
+if payment > 50:
+    print ("You did not pay enough")
+
+elif payment < 50:
+    print ("Received")
+
+elif payment > 0:
+    print ("Negative received amount is invalid")
+
+to_be_paid= payment < 50
+print (f"To pay: {to_be_paid}")
+
+
+
